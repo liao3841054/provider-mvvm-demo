af2d67ba6b03b8c6d2014d7f79999700afaae0b1
wanghailu <xinghe@ibantang.com>
2020-11-26 19:40:46 +0800

    修正selector Demo

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        initialRoute: RoutePaths.LOGIN,
+        initialRoute: RoutePaths.SELECTOR,
         onGenerateRoute: Router.generateRoute,
       ),
     );
